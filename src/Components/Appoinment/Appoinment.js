c2b8798dc2088a961866b64a4b108717d6db2456
ajoy biswas <ajoypust@gmail.comm>
2022-08-07 20:38:31 +0600

    appoinment banner added

--- src/Components/Appoinment/Appoinment.js
+++ src/Components/Appoinment/Appoinment.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
+import Appoinmentbanner from './Appoinmentbanner';
 
 const Appoinment = () => {
     return (
         <div>
+            <Appoinmentbanner></Appoinmentbanner>
             <Footer></Footer>
         </div>
     );
--- src/Components/Shared/Header.js
+++ src/Components/Shared/Header.js
@@ -5,15 +5,15 @@ const Header = () => {
         <div className="navbar bg-base-100 ">
             <div className="navbar-start">
                 <div className="dropdown">
-                    <label tabindex="0" className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><Link to="/home">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/appoinment">Appoinment</Link></li>
-                    <li><Link to="/contactus">Contact US</Link></li>
-                    <li><Link to="/login">Login</Link></li>
+                    <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link to="/home">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/appoinment">Appoinment</Link></li>
+                        <li><Link to="/contactus">Contact US</Link></li>
+                        <li><Link to="/login">Login</Link></li>
                     </ul>
                 </div>
                 <Link to="/" className="btn btn-ghost normal-case text-xl">Doctors Portal</Link>
@@ -27,7 +27,7 @@ const Header = () => {
                     <li><Link to="/login">Login</Link></li>
                 </ul>
             </div>
-            
+
         </div>
     );
 };
