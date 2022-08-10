fd1ce0f7b3cd4e99eacef1b0545757e424efa124
ajoy biswas <ajoypust@gmail.comm>
2022-08-11 02:10:11 +0600

    added email password signin

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import './Login.css'
+import './Login.css';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../../src/Firebase.init'
 const Login = () => {
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+      if(gLoading||loading){
+        return <button class="btn loading">loading</button>
+      }
+      let errorMessage;
+    if(error||gError){
+        errorMessage= <span>{error.message}</span> 
+    }
+    const handelSubmit=event=>{
+        event.preventDefault()
+        const email=event.target.email.value;
+        const password=event.target.password.value;
+        // this is email validation
+        const emailValidation = /\S+@\S+\.\S+/;
+        if(emailValidation.test(email)){
+            // console.log(email)
+        }else{
+            alert('Please Type Valid Email')
+        }
+    // this is password validation
+       const errors = [];
+        if (password.length < 8) {
+            errors.push("Your password must be at least 8 characters");
+        }
+        if (password.search(/[a-z]/i) < 0) {
+            errors.push("Your password must contain at least one letter."); 
+        }
+        if (password.search(/[0-9]/) < 0) {
+            errors.push("Your password must contain at least one digit.");
+        }
+        if (errors.length > 0) {
+            alert(errors.join("\n"));
+            return false;
+        }
+        // this is email password signin
+        signInWithEmailAndPassword(email, password)
+    }
     return (
         <div className='mx-auto w-96 login-container pt-7 pb-7 mt-10'>
             <h1 className='text-center font-bold'>Login</h1>
-            <form className='pl-10'>
+            <form onSubmit={handelSubmit} className='pl-10'>
                 <label className="label">
                     <span className="label-text">Email</span>
                 </label>
-                <input type="text" className="input input-bordered w-full max-w-xs" />
+                <input type="email"name='email'required className="input input-bordered w-full max-w-xs" />
                 <label className="label">
                     <span className="label-text">Password</span>
                 </label>
-                <input type="password" className="input input-bordered w-full max-w-xs" />
+                <input type="password"name='password'required className="input input-bordered w-full max-w-xs" />
                 <label className="label">
                     <span className="label-text">Forgot Password?</span>
                 </label>
+                <p>{errorMessage}</p>
                 <input type="submit" value="Login" className="input input-bordered w-full max-w-xs bg-accent text-white" />
+                
                 <div className="pl-8 mt-3">
                     <span className="label-text ">New to Doctor portal?<span className='text-primary'>Create New Account?</span> </span>
                 </div>
@@ -25,7 +72,7 @@ const Login = () => {
                 </div>
             </form>
             <div className='pl-10'>
-                <button className="btn w-full max-w-xs bg-accent text-white ">CONTINUE WITH GOOGLE</button>
+                <button onClick={()=>signInWithGoogle()} className="btn w-full max-w-xs bg-accent text-white ">CONTINUE WITH GOOGLE</button>
             </div>
         </div>
     );
